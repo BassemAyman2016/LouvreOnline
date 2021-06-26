b44cbfc225298858cc14c084a827ad0a313eb0df
Bassem <bassem.ayman1997@gmail.com>
2021-06-26 19:42:06 +0200

    dockerize frontend

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,9 @@ import reduxIndex from '../redux/index'
 const {store} = reduxIndex()
 require('dotenv').config()
 const api = () => {
-  var apiObject = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+  let BACKEND_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/'
+  let apiObject = axios.create({
+    baseURL: BACKEND_URL,
     headers: {
       Authorization: sessionStorage.getItem("accessToken")
     }
